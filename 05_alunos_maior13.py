8ecf613c7103ecab160ff3de80e39fdc2b2924fd
KleberJunior <kleberjrmmxvi@gmail.com>
2022-06-20 07:35:54 -0300

    ATUALIZACAO DA ULTIMA QUESTAO DA T2 DA SEMANA 13. ARREDONDAR A ALTURA

--- 05_alunos_maior13.py
+++ 05_alunos_maior13.py
@@ -3,27 +3,23 @@ idades = list()
 alturas = list()
 
 def media_func(n):
-  m = 0
+  m = 0.0
   for c in n:
     m += c
-  return m/len(n)
+  return round(m/len(n), 2)
 
 
-for c in range(3):
+for c in range(30):
   nome.append(str(input()))
   idades.append(int(input()))
   alturas.append(float(input()))
 
-
 ch = media_func(alturas)
-
-
-
 print('MAIORES DE 13 ANOS COM ALTURA ABAIXO DA MÉDIA')
-for c in range(3):
-  alturas1 = alturas[c]
-  if alturas1 < ch:
-    if idades[c] > 13:
-      print(nome[c])
-      print(ch)
-      print(alturas1)
+for c in range(30):
+  alturas1 = alturas[c] #CRIO ESSA VARIAVEL alturas1 PRA RECEBER OS VALORES DA LISTA alturas.
+  if alturas1 < ch and idades[c] > 13:
+    print(nome[c])
+
+
+
